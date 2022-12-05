7ce576efc9f6942e0267ed2e0ec869c5c2ba4ae3
kthc <kuting.church@gmail.com>
2022-12-05 12:09:29 +0800

    fix database

--- bible_database.py
+++ bible_database.py
@@ -14,6 +14,9 @@ class BibleDB:
         except:
             return False
     
+    def close(self):
+        self.con.close()
+    
     def create_userlog_table(self):
         cur = self.con.cursor()
         # listOfTables = cur.execute(
@@ -41,7 +44,8 @@ class BibleDB:
     
     def execute(self, sql):
         cur = self.con.cursor()
-        data = cur.execute(sql).fetchall()
+        cur.execute(sql)
+        data = cur.fetchall()
         return data
 
     def get_storyid_by_userid(self, useid):
@@ -52,7 +56,8 @@ class BibleDB:
         """
         cur = self.con.cursor()
         users = cur.execute(
-        f"""SELECT CurStoryID, Finished FROM UserLog WHERE UserID='{useid}'; """).fetchall()
+        f"""SELECT CurStoryID, Finished FROM UserLog WHERE UserID='{useid}'; """)
+        users = cur.fetchall()
         if len(users) > 0:
             return users[0][0]
         else:
@@ -61,8 +66,9 @@ class BibleDB:
 
     def get_finished_by_userid(self, useid):
         cur = self.con.cursor()
-        users = cur.execute(
-        f"""SELECT Finished FROM UserLog WHERE UserID='{useid}'; """).fetchall()
+        cur.execute(
+        f"""SELECT Finished FROM UserLog WHERE UserID='{useid}'; """)
+        users = cur.fetchall()
         if len(users) > 0:
             return users[0][0] == 1
         else:
@@ -71,11 +77,12 @@ class BibleDB:
 
     def add_new_user(self, useid):
         cur = self.con.cursor()
-        users = cur.execute(
-        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """).fetchall()
+        cur.execute(
+        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """)
+        users = cur.fetchall()
         if len(users) == 0:
             sql = ''' INSERT INTO UserLog(UserID,CurStoryID,Finished,LoginCount,RetryCount)
-              VALUES(?,?,?,?,?) '''
+              VALUES(%s,%s,%s,%s,%s) '''
             cur.execute(sql, (useid, 0, 0, 1,0))
             self.con.commit()
             return 1
@@ -92,42 +99,46 @@ class BibleDB:
 
     def check_user_exist(self, useid):
         cur = self.con.cursor()
-        users = cur.execute(
-        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """).fetchall()
+        cur.execute(
+        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """)
+        users = cur.fetchall()
         return len(users) > 0
 
     def update_login_count(self, useid):
         cur = self.con.cursor()
-        users = cur.execute(
-        f"""SELECT LoginCount FROM UserLog WHERE UserID='{useid}'; """).fetchall()
+        cur.execute(
+        f"""SELECT LoginCount FROM UserLog WHERE UserID='{useid}'; """)
+        users = cur.fetchall()
         if len(users) > 0:
             login_count = users[0][0]
             sql = ''' UPDATE UserLog
-              SET LoginCount = ?
-              WHERE UserID = ?'''
+              SET LoginCount = %s
+              WHERE UserID = %s'''
             cur.execute(sql, (login_count+1,useid))
             self.con.commit()
             return login_count+1
 
     def update_story_id(self, useid, storyid):
         cur = self.con.cursor()
-        users = cur.execute(
-        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """).fetchall()
+        cur.execute(
+        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """)
+        users = cur.fetchall()
         if len(users) > 0:
             sql = ''' UPDATE UserLog
-              SET CurStoryID = ?
-              WHERE UserID = ?'''
+              SET CurStoryID = %s
+              WHERE UserID = %s'''
             cur.execute(sql, (storyid,useid))
             self.con.commit()
 
     def update_finished(self, useid, finished:int):
         cur = self.con.cursor()
-        users = cur.execute(
-        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """).fetchall()
+        cur.execute(
+        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """)
+        users = cur.fetchall()
         if len(users) > 0:
             sql = ''' UPDATE UserLog
-              SET Finished = ?
-              WHERE UserID = ?'''
+              SET Finished = %s
+              WHERE UserID = %s'''
             cur.execute(sql, (finished,useid))
             self.con.commit()
 
@@ -138,8 +149,9 @@ class BibleDB:
         :return int: current retry_count, return 0 if not found this user id
         """
         cur = self.con.cursor()
-        users = cur.execute(
-        f"""SELECT RetryCount, Finished FROM UserLog WHERE UserID='{useid}'; """).fetchall()
+        cur.execute(
+        f"""SELECT RetryCount, Finished FROM UserLog WHERE UserID='{useid}'; """)
+        users = cur.fetchall()
         if len(users) > 0:
             return users[0][0]
         else:
@@ -148,27 +160,37 @@ class BibleDB:
 
     def clear_retry_count(self, useid):
         cur = self.con.cursor()
-        users = cur.execute(
-        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """).fetchall()
+        cur.execute(
+        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """)
+        users = cur.fetchall()
         if len(users) > 0:
             sql = ''' UPDATE UserLog
-              SET RetryCount = ?
-              WHERE UserID = ?'''
+              SET RetryCount = %s
+              WHERE UserID = %s'''
             cur.execute(sql, (0,useid))
             self.con.commit()
 
     def increase_1_retry_count(self, useid):
         cur = self.con.cursor()
-        users = cur.execute(
-        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """).fetchall()
+        cur.execute(
+        f"""SELECT * FROM UserLog WHERE UserID='{useid}'; """)
+        users = cur.fetchall()
         if len(users) > 0:
             cur_retry = self.get_retry_count_by_userid(useid)
             sql = ''' UPDATE UserLog
-              SET RetryCount = ?
-              WHERE UserID = ?'''
+              SET RetryCount = %s
+              WHERE UserID = %s'''
             cur.execute(sql, (cur_retry+1,useid))
             self.con.commit()
-    
+
+def test_db():
+    mydb = BibleDB()
+    mydb.connect()
+    mydb.create_userlog_table()
+    mydb.add_new_user('ony_for_test')
+    mydb.get_storyid_by_userid('ony_for_test')
+    mydb.close()
+
 db = BibleDB()
 db.connect()
 try:
@@ -177,4 +199,7 @@ try:
 except:
     pass
 db.create_userlog_table()
+
+if __name__=='__main__':
+    test_db()
 
